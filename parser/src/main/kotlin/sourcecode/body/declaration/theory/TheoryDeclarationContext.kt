4e8e37b87f79bca0a045cfc8a01c1e688bb1c0c0
Kid A <thisismeamir@outlook.com>
2025-09-09 09:19:45 +0330

    [implementation] theory declaration data class

---
+++ parser/src/main/kotlin/sourcecode/body/declaration/theory/TheoryDeclarationContext.kt
@@ -0,0 +1,8 @@
+package com.iskportal.dedukt.lang.sourcecode.body.declaration.theory
+
+import com.iskportal.dedukt.lang.sourcecode.body.Context
+
+
+data class TheoryDeclarationContext(
+    val placeHolder: String
+): Context
