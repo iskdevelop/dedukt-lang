ed4fa46df94e3b334b6f433b810f9e19744c6b4a
Kid A <thisismeamir@outlook.com>
2025-09-09 09:19:39 +0330

    [implementation] type declaration data class

---
+++ parser/src/main/kotlin/sourcecode/body/declaration/type/TypeDeclarationContext.kt
@@ -0,0 +1,7 @@
+package com.iskportal.dedukt.lang.sourcecode.body.declaration.type
+
+import com.iskportal.dedukt.lang.sourcecode.body.Context
+
+data class TypeDeclarationContext(
+    val placeHolder: String
+): Context
