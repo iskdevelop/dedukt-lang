5929cb26c573dded9a5cac53e5d2c4873ef46dab
Kid A <thisismeamir@outlook.com>
2025-09-09 09:19:50 +0330

    [implementation] structure declaration data class

---
+++ parser/src/main/kotlin/sourcecode/body/declaration/structure/StructureDeclarationContext.kt
@@ -0,0 +1,7 @@
+package com.iskportal.dedukt.lang.sourcecode.body.declaration.structure
+
+import com.iskportal.dedukt.lang.sourcecode.body.Context
+
+data class StructureDeclarationContext (
+    val placeHolder: String
+): Context
